41a42daf51fb0f284b4970c24e337fd1fcd5542d
Anubhav311 <anubhavbhambri1@gmail.com>
2019-07-12 03:50:05 +0530

    completed MVP for day 2

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
@@ -40,7 +41,6 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
-player = Player("Player 1", room["outside"])
 
 # Write a loop that:
 #
@@ -49,29 +49,62 @@ player = Player("Player 1", room["outside"])
 # * Waits for user input and decides what to do.
 #
 
-while True:
-    print("room name: ", getattr(player.current_room, "name"))
-    print("description: ", player.current_room.description, "\n")
-
-    direction = input("provide direction e/w/n/s -> ") 
-
-    if direction == "q":
-        player.current_room = room["outside"]
-        print("Adios...!!!", "\n")
-        break
-
-    elif direction != "e" and direction != "w" and direction != "n" and direction != "s":
-        print("please provide a valid input", "\n")
-
-    elif getattr(player.current_room, f"{direction}_to") == "nope":
-        print("Sorry! there's no room here.", "\n")
-
-    else:
-        player.current_room = getattr(player.current_room, f"{direction}_to")
-
-
-
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+rock = Item("Rock", "this is a rock")
+pencil = Item("Pencil", "this is a pencil")
+sandwich = Item("Sandwich", "this is delicious sandwich")
+
+player = Player("Player 1", room["outside"])
+
+player.items.append(rock)
+room['outside'].items.append(pencil)
+room['outside'].items.append(sandwich)
+
+current_room = player.current_room
+
+print(current_room)
+
+valid_directions = ["n", "s", "e", "w"]
+
+while True:
+    cmd = input("provide direction e/w/n/s -> ")
+
+    if cmd in valid_directions:
+        player.travel(cmd)
+    elif cmd == "i":
+        player.print_inventory()
+    elif cmd == "q":
+        print("Goodbye!")
+        exit()
+    else:
+        print("I did not recognise that command")
+
+
+
+
+# while True:
+#     print("room name: ", getattr(player.current_room, "name"))
+#     print("description: ", player.current_room.description, "\n")
+
+#     direction = input("provide direction e/w/n/s -> ") 
+
+#     if direction == "q":
+#         player.current_room = room["outside"]
+#         print("Adios...!!!", "\n")
+#         break
+
+#     elif direction != "e" and direction != "w" and direction != "n" and direction != "s":
+#         print("please provide a valid input", "\n")
+
+#     elif getattr(player.current_room, f"{direction}_to") == "nope":
+#         print("Sorry! there's no room here.", "\n")
+
+#     else:
+#         player.current_room = getattr(player.current_room, f"{direction}_to")
+
+
+
